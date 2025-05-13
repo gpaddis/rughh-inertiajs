133d87f981640edda30a2e38bf3972c85e2deb85
Gianpiero Addis <gianpiero.addis@protonmail.com>
2025-05-13 16:32:57 +0200

    Color on hover

--- app/frontend/pages/Layout/Navbar.tsx
+++ app/frontend/pages/Layout/Navbar.tsx
@@ -5,31 +5,31 @@ export default function Navbar() {
     <div className="font-semibold text-lg">
       <ul className="flex items-center space-x-2">
         <li>
-          <Link href="/posts">
+          <Link href="/posts" className="hover:text-blue-600">
             Form Helpers
           </Link>
         </li>
         <li>&middot;</li>
         <li>
-          <Link href="/demo/defer">
+          <Link href="/demo/defer" className="hover:text-blue-600">
             Defer
           </Link>
         </li>
         <li>&middot;</li>
         <li>
-          <Link href="/demo/load-when-visible">
+          <Link href="/demo/load-when-visible" className="hover:text-blue-600">
             Load when visible
           </Link>
         </li>
         <li>&middot;</li>
         <li>
-          <Link href="/demo/partial-reloads">
+          <Link href="/demo/partial-reloads" className="hover:text-blue-600">
             Partial reloads
           </Link>
         </li>
         <li>&middot;</li>
         <li>
-          <Link href="/demo/prefetch">
+          <Link href="/demo/prefetch" className="hover:text-blue-600">
             Prefetch
           </Link>
         </li>
