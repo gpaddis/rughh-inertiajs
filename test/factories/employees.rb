e9d8a1c975641c247f1308c37005b0f7c2203e75
Gianpiero Addis <gianpiero.addis@protonmail.com>
2025-05-14 17:12:03 +0200

    Use unique values

--- test/factories/employees.rb
+++ test/factories/employees.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :employee do
-    name { Faker::Artist.name }
-    role { Faker::Job.title }
+    name { Faker::Artist.unique.name }
+    role { Faker::Job.unique.title }
     status { [ "active", "inactive" ].sample }
   end
 end
